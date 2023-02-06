463b28fcb14f2bb3a5f1fef334bc83b10fc9867b
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-02-06 15:03:22 +0000

    norm for read_file.c

--- FdF/srcs/read_file.c
+++ FdF/srcs/read_file.c
@@ -6,11 +6,32 @@
 /*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 11:45:14 by diogmart          #+#    #+#             */
-/*   Updated: 2023/02/06 14:11:31 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/02/06 15:01:06 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+/* 
+void	ft_printmap(t_data *data)
+{
+	int i = 0;
+	int h = 0;
+	while(h < data->height)
+	{
+		i = 0;
+		while (i < data->width)
+		{
+			if (data->map[h][i] == 10)
+				ft_printf("%d ", data->map[h][i]);
+			else if (data->map[h][i] == 0)
+				ft_printf("%d  ", data->map[h][i]);
+			i++;
+		}
+		ft_printf("\n");
+		h++;
+	}
+} */
 
 int	get_width(char *file_name)
 {
@@ -43,48 +64,47 @@ int	get_height(char *file_name)
 	return (height);
 }
 
-int	**fill_map(t_data *data, char *file_name)
+void	fill_row(int *row, char *line)
 {
-	int		fd;
-	int		i;
-	int		j;
-	int		h;
-	int		**map;
-	char	*line;
+	int	i;
+	int	j;
 
-	j = 0;
 	i = 0;
-	h = 0;
-	map = (int **)ft_calloc(sizeof(int *), (data->width + 1));
-	fd = open(file_name, 'r');
-	line = get_next_line(fd);
-	while (h < data->width)
-		map[h++] = (int *)ft_calloc(sizeof(int), (data->height + 1));
-	while (line != NULL && i < data->height)
+	j = 0;
+	while (line[j] != '\0')
 	{
-		h = 0;
-		while (line[j] != '\0')
+		if (ft_isdigit(line[j]))
 		{
-			if (ft_isdigit(line[j]))
-			{
-				map[h++][i] = ft_atoi(&line[j]);
-				j += ft_nbr_len(ft_atoi(&line[j]));
-			}
-			else
-				j++;
+			row[i++] = ft_atoi(&line[j]);
+			j += ft_nbr_len(ft_atoi(&line[j]));
 		}
-		i++;
-		j = 0;
-		free(line);
-		line = get_next_line(fd);
+		else
+			j++;
 	}
-	close(fd);
-	return (map);
 }
 
 void	read_file(t_data *data, char *file_name)
 {
+	char	*line;
+	int		fd;
+	int		i;
+	int		h;
+
 	data->height = get_height(file_name);
 	data->width = get_width(file_name);
-	data->map = fill_map(data, file_name);
+	fd = open(file_name, 'r');
+	i = 0;
+	h = 0;
+	data->map = (int **)ft_calloc(sizeof(int *), (data->height + 1));
+	line = get_next_line(fd);
+	while (h < data->width)
+		data->map[h++] = (int *)ft_calloc(sizeof(int), (data->width + 1));
+	while (line != NULL && i < data->height)
+	{
+		fill_row(data->map[i++], line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	free(line);
+	close(fd);
 }
