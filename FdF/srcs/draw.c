4d07831b31bc4d888f2957ddcb38386b83411fce
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-02-15 14:09:19 +0000

    first steps thowards 3D

--- FdF/srcs/draw.c
+++ FdF/srcs/draw.c
@@ -6,11 +6,12 @@
 /*   By: diogmart <diogmart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 11:09:32 by diogmart          #+#    #+#             */
-/*   Updated: 2023/02/15 11:43:27 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/02/15 13:43:06 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include <stdio.h>
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -18,6 +19,20 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
+}
+
+void	ft_isometric(float *x, float *y, int z)
+{
+	float	x0;
+	float	y0;
+
+	x0 = *x;
+	y0 = *y;
+	
+/* 	*x = x0 * cos(0.8) - y0 * sin(0.8);
+	*y = x0 * sin(0.8) + y0 * cos(0.8) + z; */
+	*x = (*x - *y) * cos(0.8);
+	*y = (x0 + *y) * sin(0.8) - z;
 }
 
 void	ft_bresenham(t_data *data, float x0, float y0, float x1, float y1)
@@ -39,22 +54,32 @@ void	ft_bresenham(t_data *data, float x0, float y0, float x1, float y1)
 
 	x_step = x1 - x0;
 	y_step = y1 - y0;
+
+	ft_isometric(&x0, &y0, z);
+	ft_isometric(&x1, &y1, z1);
+	x0 += 150;
+	y0 += 150;
+	x1 += 150;
+	y1 += 150;
+
+
 	max = ft_max(module(x_step), module(y_step));
 	x_step /= max;
 	y_step /= max;
-	while ((int)(x0 - x1) || (int)(y0 - y1))
+	while ((int)(x0 - x1) && (int)(y0 - y1))
 	{
-		my_mlx_pixel_put(data, x0, y0, data->color);
+		//my_mlx_pixel_put(data, x0, y0, data->color);
+		mlx_pixel_put(data->mlx, data->mlx_win, x0, y0, data->color);
 		x0 += x_step;
 		y0 += y_step;
 	}
-	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img, 0, 0);
+	//mlx_put_image_to_window(data->mlx, data->mlx_win, data->img, 0, 0);
 }
 
 void	draw(t_data *data)
 {
-	int	y;
-	int	x;
+	float	y;
+	float	x;
 
 	y = 0;
 	while (y < data->height)
@@ -63,9 +88,9 @@ void	draw(t_data *data)
 		while (x < data->width)
 		{
 			if (x < data->width - 1)
-				ft_bresenham(data, x, y, x + 1, y);
+				ft_bresenham(data, (float)x, (float)y, (float)(x + 1), (float)y);
 			if (y < data->height - 1)
-				ft_bresenham(data, x, y, x, y + 1);
+				ft_bresenham(data, (float)x, (float)y, (float)x, (float)(y + 1));
 			x++;
 		}
 		y++;
